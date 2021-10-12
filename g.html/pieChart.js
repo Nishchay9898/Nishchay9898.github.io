76ed579b99e021f3d523667485ad6b38e49ce496
Nishchay9898 <59975877+Nishchay9898@users.noreply.github.com>
2021-10-12 01:45:39 -0400

    Add files via upload

---
+++ g.html/pieChart.js
@@ -0,0 +1,63 @@
+(function pieChart() {
+
+    var pieChart = d3.select("#pieChart"),
+        width = pieChart.attr("width"),
+        height = pieChart.attr("height"),
+        radius = Math.min(width, height) / 2;
+
+    var g = pieChart.append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c', 'rgb(223,151,172)']);
+
+
+    var pie = d3.pie().value(function (d) {
+        return d.deaths;
+    });
+
+    var path = d3.arc()
+        .outerRadius(radius - 15)
+        .innerRadius(0);
+
+    var label = d3.arc()
+        .outerRadius(radius)
+        .innerRadius(radius - 110);
+
+    d3.csv("data/By_Age.csv", function (error, data) {
+        if (error) {
+            throw error;
+        }
+        var arc = g.selectAll(".arc")
+            .data(pie(data))
+            .enter().append("g")
+            .attr("class", "arc");
+
+        arc.append("path")
+            .attr("d", path)
+            .attr("fill", function (d) {
+                return color(d.data.age);
+            });
+
+        console.log(arc)
+
+        arc.append("text")
+            .attr("transform", function (d) {
+                return "translate(" + label.centroid(d) + ")";
+            })
+            .text(function (d) {
+                return "[" + d.data.age + "]" + " " + d.data.deaths;
+            })
+
+    });
+
+    pieChart.append("g")
+        .attr("transform", "translate(" + (width / 2 - 180) + "," + 12 + ")")
+        .append("text")
+        .attr("y", 2)
+        .attr("x", -20)
+        .text("Death By Age")
+        .attr("class", "title")
+        .style("font-weight","bold")
+        .style('font-size', 15)
+        
+}());
